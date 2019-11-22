9f25199dc4e4f6fa284fdde7bc5f3bbf3aecde2a
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-11-22 08:24:44 -0800

    add images and kombucha component

--- src/Components/NewKombucha.jsx
+++ src/Components/NewKombucha.jsx
@@ -24,15 +24,29 @@ function NewKombucha() {
       </div>
       <div style={form} className="form-group col-md-6">
         <h1>Add a new Kombucha: </h1><br />
-        <input type="text" placeholder="Name" className="form-control" /><br />
-        <input type="text" placeholder="Flavour" className="form-control" /><br />
-        <input type="text" placeholder="Price" className="form-control" /><br />
-        <select className="form-control">
-          <option selected>Brand...</option>
-          <option>GT's</option>
-          <option>Remedy</option>
-        </select><br />
-        <button type="submit" className="btn btn-secondary">Add</button><hr />
+        <form onSubmit={handleSubmission}>
+          <input
+            type='text'
+            id='name'
+            placeholder='name'
+            ref={(input) => { _name = input; }} />
+          <input
+            type='text'
+            id='price'
+            placeholder='price'
+            ref={(input) => { _price = input; }} />
+          <input
+            type='text'
+            id='flavour'
+            placeholder='flavour'
+            ref={(input) => { _flavour = input; }} />
+          {/* <select className="form-control" ref={(select) => { _tamo = select; }}>
+            <option selected> Select your Brand...</option>
+            <option value='monster'  >Gt's</option>
+            <option value='alien'>Remedy</option>
+          </select><br /> */}
+          <button type='submit'>create player</button>
+        </form>
         <nav class="navbar navbar-light bg-light">
           <a class="navbar-brand" href="#">
             <button className="btn btn-secondary">Home</button>
