38decd9b51c142e574ff38f34f3a7a4b99a43630
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-11-22 16:33:18 -0800

    change title name in npm files

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'template.ejs',
       appMountId: 'react-app-root',
-      title: 'Vote Site',
+      title: 'Kombucha',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
